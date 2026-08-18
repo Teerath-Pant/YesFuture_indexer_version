2df4643c29e87a8aa55ab4d271b21c3a54244991
root <root@srv1777451.hstgr.cloud>
2026-08-18 10:05:08 +0000

    change abi

--- user/src/lib/auth-api.ts
+++ user/src/lib/auth-api.ts
@@ -1627,7 +1627,7 @@ const CORE_ABI = [
     ],
     "stateMutability": "view"
   }
-]
+] 
 const USDT_ABI = [
   "function approve(address spender, uint256 amount) external returns (bool)",
   "function allowance(address owner, address spender) external view returns (uint256)",
